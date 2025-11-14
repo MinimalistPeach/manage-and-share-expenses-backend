8ff6efcb121d7c83afb1910e83cfe9f1f87a81a9
MinimalistPeach <stremler.lacika77@gmail.com>
2025-11-14 18:05:09 +0100

    Spring-Boot skeleton

---
+++ src/test/java/hu/mase/mase_backend/MaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.mase.mase_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
