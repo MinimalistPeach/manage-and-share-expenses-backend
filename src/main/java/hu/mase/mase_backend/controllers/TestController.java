83224ff59dc840f0a6161567902f54011fdc1c92
MinimalistPeach <stremler.lacika77@gmail.com>
2025-11-14 18:39:34 +0100

    Adding Tesseract OCR

--- src/main/java/hu/mase/mase_backend/controllers/TestController.java
+++ src/main/java/hu/mase/mase_backend/controllers/TestController.java
@@ -1,14 +1,30 @@
 package hu.mase.mase_backend.controllers;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import hu.mase.mase_backend.services.TesseractService;
 
 @RestController
 public class TestController {
 
+    private final TesseractService tesseractService;
+
+    public TestController(TesseractService tesseractService) {
+        this.tesseractService = tesseractService;
+    }
+
     @GetMapping("/test")
-    public String testEndpoint() {
-        return "The backend is running!";
+    public String testEndpoint() throws FileNotFoundException, IOException {
+        InputStream fileInputStream = new FileInputStream(new File("src/main/resources/static/kep.jpg"));
+        String result = tesseractService.recognizeText(fileInputStream);
+        return result;
     }
 
 }
---
+++ src/main/java/hu/mase/mase_backend/services/TesseractService.java
@@ -0,0 +1,32 @@
+package hu.mase.mase_backend.services;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+@Service
+public class TesseractService {
+
+    @Autowired
+    private Tesseract tesseract;
+
+    public String recognizeText(InputStream inputStream) throws IOException {
+        BufferedImage image = ImageIO.read(inputStream);
+        try {
+            tesseract.setLanguage("hun");
+            return tesseract.doOCR(image);
+        } catch (TesseractException e) {
+            e.printStackTrace();
+        }
+        return "failed";
+    }
+
+}
